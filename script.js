2f1f1fda8922bf45024e15affbeac213538b95c5
Secondaryaccount1 <secondariogiulio@gmail.com>
2025-05-13 22:26:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -386,3 +386,103 @@ const questions = [
     ]
   },
 ];
+
+
+let randomQuestions = shuffle(questions.slice());
+
+
+const questionElement = document.getElementById("question");
+  const answerButtons = document.getElementById("answer-buttons");
+  const nextButton = document.getElementById("next-btn");
+  
+  let currentQuestionIndex = 0;
+  let score = 0;
+  
+  function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+  }
+  
+  function showQuestion(){
+    resetState();
+    let currentQuestion = randomQuestions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+  
+    currentQuestion.answers.forEach(answer => {
+      const button = document.createElement("button");
+      button.innerHTML = answer.text;
+      button.classList.add("btn");
+      answerButtons.appendChild(button);
+      if(answer.correct){
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener("click", selectAnswer);
+    })
+  }
+  
+  function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+      answerButtons.removeChild(answerButtons.firstChild);
+    }
+  }
+  
+  function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct == "true";
+    if(isCorrect){
+      selectedBtn.classList.add("correct");
+      score++
+    }else{
+      selectedBtn.classList.add("incorrect")
+    }
+  
+    Array.from(answerButtons.children).forEach(button =>{
+      if(button.dataset.correct == "true"){
+        button.classList.add("correct");
+      }
+      button.disabled = true;
+      
+    });
+    nextButton.style.display = "block";
+    
+  }
+  
+  function showScore(){
+    resetState();
+    questionElement.innerHTML = `Hai ottenuto un punteggio di ${score} su 20!`;
+    nextButton.innerHTML = "Gioca Ancora";
+    nextButton.style.display = "block"
+  }
+  
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < 20) { // Change 20 to the desired number of questions
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+  
+  
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+      handleNextButton();
+    }else{
+      startQuiz();
+    }
+  }); 
+  
+  startQuiz();
